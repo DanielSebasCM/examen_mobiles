47779bf1307c31676b0880b99eb370a68ab05ef0
Daniel Cajas <danielsebascm@hotmail.com>
2023-10-10 12:26:23 -0600

    Can view movie images

--- app/src/main/java/com/example/examen/framework/views/activities/SplashscreenActivity.kt
+++ app/src/main/java/com/example/examen/framework/views/activities/SplashscreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.examen.framework.views.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.examen.databinding.ActivitySplashScreenBinding
 import com.example.examen.utils.Constants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,6 +14,7 @@ class SplashscreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstances: Bundle?) {
         super.onCreate(savedInstances)
+        setContentView(ActivitySplashScreenBinding.inflate(layoutInflater).root)
         CoroutineScope(Dispatchers.Main).launch {
             delay(Constants.SPLASHSCREEN_DURATION)
             passViewGoToMain()
--- app/src/main/java/com/example/examen/utils/Constants.kt
+++ app/src/main/java/com/example/examen/utils/Constants.kt
@@ -3,6 +3,7 @@ package com.example.examen.utils
 object Constants {
     const val SPLASHSCREEN_DURATION = 3000L
     const val SERVER_BASE_URL = "https://api.themoviedb.org/"
+    const val IMAGE_SERVER_URL = "https://image.tmdb.org/t/p/original"
     const val TOKEN =
         "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjhkZDc1NmUzMmI4YWNlZjYyZmQ2YzMwZmQwY2NmOSIsInN1YiI6IjY0ZWI5MzhiZTg5NGE2MDEzYmIxNjNjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6cGDTI8wql15qnVZErrd_6QRNaiRAi74pRD0LfOzVZM"
 }
